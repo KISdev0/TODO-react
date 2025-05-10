b53bb5eb31f51d4801c92f5c8dd7248b7c5cfa9c
Ivan <kleninivan01@mail.ru>
2025-05-10 16:58:41 +0300

    добавили категории

--- src/ui/TodoList/TodoList.tsx
+++ src/ui/TodoList/TodoList.tsx
@@ -1,18 +1,54 @@
 import { TodoListProps } from "../../Types";
 import { TodoItem } from "../TodoItem/TodoItem";
+import { Box, Typography } from "@mui/material";
 
-export const TodoList = ({ todos, onDelete, onToggle, onEdit }: TodoListProps) => {
+export const TodoList = ({
+  todos,
+  onDelete,
+  onToggle,
+  onEdit,
+}: TodoListProps) => {
+  const activeTodos = todos.filter((todo) => !todo.completed);
+  const completedTodos = todos.filter((todo) => todo.completed);
+
   return (
-    <ul>
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onDelete={onDelete}
-          onToggle={onToggle}
-          onEdit={onEdit}
-        />
-      ))}
-    </ul>
+    <Box>
+      {activeTodos.length > 0 && (
+        <Box>
+          <Typography variant="h6" component="h2" gutterBottom>
+            План({activeTodos.length})
+          </Typography>
+          <ul>
+            {activeTodos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                onDelete={onDelete}
+                onToggle={onToggle}
+                onEdit={onEdit}
+              />
+            ))}
+          </ul>
+        </Box>
+      )}
+      {completedTodos.length > 0 && (
+        <Box>
+          <Typography variant="h6" component="h2" gutterBottom>
+            Готово({completedTodos.length})
+          </Typography>
+          <ul>
+            {completedTodos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                onDelete={onDelete}
+                onToggle={onToggle}
+                onEdit={onEdit}
+              />
+            ))}
+          </ul>
+        </Box>
+      )}
+    </Box>
   );
 };
